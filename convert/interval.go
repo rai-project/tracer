3eb6f7461f8c17118b1b538ebcb8017b2788270c
Abdul Dakkak <abduld@wolfram.com>
2019-04-02 23:54:52 -0500

    fix

--- convert/interval.go
+++ convert/interval.go
@@ -50,8 +50,7 @@ func (i Interval) Contains(iv Interval) bool {
 	if iv.Span == nil {
 		return false
 	}
-	// return i.Start() <= iv.Start() && iv.End() <= i.End()
-	return i.OverlapsAtDimension(iv, 0)
+	return i.Start() <= iv.Start() && iv.End() <= i.End()
 }
 
 func (i Interval) LowAtDimension(uint64) int64 {
@@ -66,10 +65,10 @@ func (i Interval) ID() uint64 {
 	return hash(string(i.SpanID))
 }
 
-// func (i Interval) OverlapsAtDimension(iv augmentedtree.Interval, dimension uint64) bool {
-// 	return i.HighAtDimension(dimension) > iv.HighAtDimension(dimension) &&
-// 		i.LowAtDimension(dimension) < iv.LowAtDimension(dimension)
-// }
+func (i Interval) OverlapsAtDimension(iv augmentedtree.Interval, dimension uint64) bool {
+	return i.HighAtDimension(dimension) > iv.HighAtDimension(dimension) &&
+		i.LowAtDimension(dimension) < iv.LowAtDimension(dimension)
+}
 
 // func (i Interval) OverlapsAtDimension(iv augmentedtree.Interval, dimension uint64) bool {
 // 	if i.Span == nil {
@@ -82,30 +81,30 @@ func (i Interval) ID() uint64 {
 // 		i.LowAtDimension(dimension) < iv.HighAtDimension(dimension)
 // }
 
-func (ci Interval) OverlapsAtDimension(iv0 augmentedtree.Interval, dim uint64) bool {
-	if ci.Span == nil {
-		return false
-	}
-	iv := iv0.(Interval)
-	if iv.Span == nil {
-		return false
-	}
+// func (ci Interval) OverlapsAtDimension(iv0 augmentedtree.Interval, dim uint64) bool {
+// 	if ci.Span == nil {
+// 		return false
+// 	}
+// 	iv := iv0.(Interval)
+// 	if iv.Span == nil {
+// 		return false
+// 	}
 
-	if (uint64(iv.LowAtDimension(dim)) <= ci.Start()) && (ci.End() <= uint64(iv.HighAtDimension(dim))) {
-		// self       ================
-		// other   =====================
-		return true
-	} else if (ci.Start() <= uint64(iv.LowAtDimension(dim))) && (uint64(iv.LowAtDimension(dim)) <= ci.End()) {
-		// self      ================
-		// other         ===============
-		return true
-	} else if (ci.Start() <= uint64(iv.HighAtDimension(dim))) && (uint64(iv.HighAtDimension(dim)) <= ci.End()) {
-		// self      ===============
-		// other  =================
-		return true
-	}
-	return false
-}
+// 	if (uint64(iv.LowAtDimension(dim)) <= ci.Start()) && (ci.End() <= uint64(iv.HighAtDimension(dim))) {
+// 		// self       ================
+// 		// other   =====================
+// 		return true
+// 	} else if (ci.Start() <= uint64(iv.LowAtDimension(dim))) && (uint64(iv.LowAtDimension(dim)) <= ci.End()) {
+// 		// self      ================
+// 		// other         ===============
+// 		return true
+// 	} else if (ci.Start() <= uint64(iv.HighAtDimension(dim))) && (uint64(iv.HighAtDimension(dim)) <= ci.End()) {
+// 		// self      ===============
+// 		// other  =================
+// 		return true
+// 	}
+// 	return false
+// }
 
 func spanToInterval(s model.Span) Interval {
 	return Interval{&s}
